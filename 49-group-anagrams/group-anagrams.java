26cf07070d53bc8746cb1c7cbbcaae30b0722c3f
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-08-20 00:33:45 +0530

    Time: 7 ms (72.44%) | Memory: 50 MB (20.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        int n = strs.length;
+
+        for(String s:strs){
+            char ch[] = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+
+            if(map.containsKey(key)){
+                map.get(key).add(s);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(key, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
