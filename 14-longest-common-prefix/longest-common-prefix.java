b41fef4e08e1888f10773f732217a10358343a16
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-09-01 19:42:26 +0530

    Time: 1 ms (60.56%) | Memory: 43 MB (82.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -14,9 +14,7 @@ class Solution {
         while(i<first.length() && i<last.length() && first.charAt(i) == last.charAt(i)){
             i++;
         }
-        for(int j=0;j<i;j++){
-            ans+=first.charAt(j);
-        }
+        ans = first.substring(0,i);
 
         return ans;
     }
