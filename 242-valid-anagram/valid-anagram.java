6c4da858e44c1b5efbb5bdb9694ff06cc409ee1e
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-09-01 23:29:21 +0530

    Time: 4 ms (83.52%) | Memory: 44.2 MB (82.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int n = s.length();
+        if(s.length()!=t.length()) return false;
 
+        int n = s.length();
         int freq[] = new int[26];
+
         for(int i=0;i<n;i++){
             freq[s.charAt(i)-'a']++;
             freq[t.charAt(i)-'a']--;
@@ -12,6 +13,7 @@ class Solution {
         for(int num:freq){
             if(num!=0) return false;
         }
+
         return true;
     }
 }
