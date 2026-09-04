8c313435e92b5feb214dae39de341a611778c6c8
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-09-04 22:49:53 +0530

    Time: 2 ms (89.02%) | Memory: 47.9 MB (42.81%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,12 +3,12 @@ class Solution {
         int maxIndex = 0;
 
         for(int i=0;i<nums.length;i++){
-
             if(i>maxIndex) return false;
 
-            maxIndex = Math.max(nums[i]+i,maxIndex);
-            
+            maxIndex = Math.max(maxIndex, nums[i]+i);
+            if(maxIndex > nums.length) return true;
         }
+
         return true;
     }
 }
