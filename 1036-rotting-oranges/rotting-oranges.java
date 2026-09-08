6fe7503f30a3c84e58cd769bc5bc31cf187b9539
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-09-08 09:03:01 +0530

    Time: 2 ms (88.65%) | Memory: 44.1 MB (71.83%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -12,24 +12,26 @@ class Solution {
     }
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length; 
+        int m = grid[0].length;
+
         int vis[][] = new int[n][m];
+
         Queue<Pair> q = new LinkedList<Pair>();
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
                     q.add(new Pair(i,j,0));
-                    vis[i][j] = 2;
+                    vis[i][j] =2;
                 }else{
-                    vis[i][j] = 0;
+                    vis[i][j] = 1;
                 }
             }
         }
 
-        int tm=0;
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,-1,0,1};
+        int tm = 0;
 
         while(!q.isEmpty()){
             int r = q.peek().first;
@@ -41,22 +43,23 @@ class Solution {
             tm = Math.max(t,tm);
 
             for(int i=0;i<4;i++){
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
+                int newrow = r + delrow[i];
+                int newcol = c + delcol[i];
 
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m
-                && vis[nrow][ncol] !=2 && grid[nrow][ncol] == 1){
-                    vis[nrow][ncol] = 2;
-                    q.add(new Pair(nrow,ncol,t+1));
+                if(newrow >=0 && newrow < n && newcol >=0 && newcol < m &&
+                vis[newrow][newcol] !=2 && grid[newrow][newcol]==1){
+                    vis[newrow][newcol] =2;
+                    q.add(new Pair(newrow,newcol,t+1));
                 }
             }
-
         }
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(vis[i][j] != 2 && grid[i][j] == 1) return -1;
-        }
-       }
-       return tm;
+         for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(vis[i][j] !=2  && grid[i][j]==1 ){
+                        return -1;
+                    }
+                }
+            }
+        return tm;
     }
 }
