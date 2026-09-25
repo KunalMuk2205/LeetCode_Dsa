e522d24bc86641e0f5f14bc6571e39edc9834972
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-09-25 18:32:56 +0530

    Time: 53 ms (77.75%) | Memory: 68.5 MB (87.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,16 +7,16 @@ class Solution {
         while(r<n){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
 
-            while(map.size() > 2){
+            if(map.size()>2){
                 map.put(fruits[l],map.get(fruits[l])-1);
-
-                if(map.get(fruits[l]) == 0){
-                    map.remove(fruits[l]);
-                }
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
                 l++;
             }
-            int len = r-l+1;
-            maxLen = Math.max(len,maxLen);
+
+            if(map.size()<=2){
+                int len = r-l+1;
+                maxLen = Math.max(len,maxLen);
+            }
             r++;
         }
         return maxLen;
